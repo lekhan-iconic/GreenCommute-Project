f5a4b8e657ad171a29fd9adf0c93cc25108e8315
Narra Lekhan Kumar <132564594+lekhan-iconic@users.noreply.github.com>
2024-10-07 11:51:35 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -24,6 +24,6 @@ const uri = "mongodb://127.0.0.1:27017/auth";
 
 mongoose.connect(uri);
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server is Running");
 });
